6c2f109086e58ab9ccf90548b6ad04220ddd0ab1
Thomas Lutz <tlutz65@gmail.com>
2023-04-02 11:05:06 -0700

    feat: home page and install styled components

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,3 +1,28 @@
+import { Link } from "react-router-dom";
+import { HomePageContainer } from "./styles";
+
 export const Home = () => {
-  return <div>Home </div>;
+  const authorized = false;
+
+  if (!authorized) {
+    return (
+      <HomePageContainer>
+        <h1>Home</h1>
+        <section>
+          Welcome to Tom's Hydra Prompt. Please
+          <Link to="/signin"> Sign In </Link>
+          or
+          <Link to="/signup"> Sign Up </Link>
+          or Create an Account to continue
+        </section>
+      </HomePageContainer>
+    );
+  }
+
+  return (
+    <HomePageContainer>
+      <h1>Home</h1>
+      <section>Welcome to Tom's Hydra Prompt. Here is your content!</section>
+    </HomePageContainer>
+  );
 };
